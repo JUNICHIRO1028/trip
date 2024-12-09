43640891c76ef00e30c625a8f0429a4919eb35e0
EC2 Default User <ec2-user@ip-172-31-47-168.eu-north-1.compute.internal>
2024-12-09 09:50:09 +0000

    trip

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
