8db0e62fc3943d2ae2e4cf821340f9fc421d4568
EC2 Default User <ec2-user@ip-172-31-47-168.eu-north-1.compute.internal>
2024-12-10 11:42:03 +0000

    trip

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,10 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
