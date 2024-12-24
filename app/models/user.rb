0c9050462084458b2037e17dd59f9057f3febd6c
EC2 Default User <junjun091028@gmail.com>
2024-12-24 12:26:44 +0000

    tripiwate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  devise authentication_keys: [:email]
+
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :group_users, dependent: :destroy
@@ -31,13 +33,5 @@ class User < ApplicationRecord
     end
   end
 
-  GUEST_USER_EMAIL = "guest@example.com"
-
-  def self.guest
-    find_or_create_by(email: GUEST_USER_EMAIL) do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
-    end
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
 
-  root to: 'homes#top'
-
-  get 'homes/about', to: 'homes#about', as: :about
-
-  devise_for :users
-
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
-  
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create, :destroy]
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+    resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+    get "search" => "searches#search"
   end
-  
-  resources :users, only: [:show, :edit, :update]
 
-  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
 
-  get "search" => "searches#search"
 end
