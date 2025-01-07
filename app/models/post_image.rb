2c6a4aec94a77b4d5ef288da968d764292db645d
EC2 Default User <junjun091028@gmail.com>
2025-01-07 03:31:38 +0000

    tripiwate

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,7 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :post_image_tags, dependent: :destroy
   has_many :tags, through: :post_image_tags, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
@@ -34,6 +35,10 @@ class PostImage < ApplicationRecord
     else
       @post_image = PostImage.all
     end
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
   
 end
